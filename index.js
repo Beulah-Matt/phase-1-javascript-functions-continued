d6dc19552d27cda7789459142c4d7aedff66118d
Beulah Mathenge <beulahmathenge@gmail.com>
2022-08-17 15:54:50 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(string = 'roller-skate'){
+    return `This Saturday, I want to ${string}!`
+} 
+ saturdayFun();
+
+ const mondayWork = (function(string = 'go to the office'){
+    return `This Monday, I will ${string}.`;
+ });
+ mondayWork();
+
+ function wrapAdjective(string){
+    return function(adjective= 'special'){
+        return `You are ${string}${adjective}${string}!`
+    }
+ }
